1bd3445b4cb45b869acc45d62f89f8948b9d45e3
Davis Lang <s3775656@student.rmit.edu.au>
2023-05-14 21:01:49 +1000

    Update sketch.js

--- public/page/sketch.js
+++ public/page/sketch.js
@@ -6,79 +6,75 @@ let bgToggle = true;
 //Create an audio context
 const audio_context = new AudioContext();
 
-//Set bgtoggle to true
-
-
-// Define function to load audio file asynchronously (this is something that was suggested to me by vscode i originally had it set up as a normal function)
+// Define function to load audio file asynchronously
 const loadAudio = async (audioFiles) => {
-  const audioIndex = Math.floor(Math.random() * audioFiles.length);
-  const response = await fetch(audioFiles[audioIndex]);
-  const arrayBuffer = await response.arrayBuffer();
-  const audioBuffer = await audio_context.decodeAudioData(arrayBuffer);
-  return audioBuffer;   
+    const audioIndex = Math.floor(Math.random() * audioFiles.length);
+    const response = await fetch(audioFiles[audioIndex]);
+    const arrayBuffer = await response.arrayBuffer();
+    const audioBuffer = await audio_context.decodeAudioData(arrayBuffer);
+    return audioBuffer;
 };
 
-// Call the function with possible audio files & get the promise(I wanted to include an array for that sweet sweet mark)
+// Call the function with possible audio files & get the promise
 const audioBufferPromise = loadAudio(["drone.wav", "drone_1.wav"]);
 
 // On setup, initialize the canvas
 function setup() {
-  createCanvas(innerWidth, innerHeight);
+    createCanvas(innerWidth, innerHeight);
 
-  // Attach an asynchronous click event listener to the canvas
-  canvas.addEventListener("click", async () => {
-    const buffer = await audioBufferPromise;
-    const source = audio_context.createBufferSource();
-    const gainNode = audio_context.createGain();
-    source.buffer = buffer;
-    source.connect(gainNode); // connect the source to the gain node
-    gainNode.connect(audio_context.destination); // connect the gain node to the destination
-    gainNode.gain.value = 0.09; //setting gain to 0.9 
-    source.start(0);
-    source.loop = true; //loop audio so you can stare for longer \/O_O\/
-  });
+    // Attach an asynchronous click event listener to the canvas
+    canvas.addEventListener("click", async () => {
+        const buffer = await audioBufferPromise;
+        const source = audio_context.createBufferSource();
+        const gainNode = audio_context.createGain();
+        source.buffer = buffer;
+        source.connect(gainNode); // connect the source to the gain node
+        gainNode.connect(audio_context.destination); // connect the gain node to the destination
+        gainNode.gain.value = 0.09; //setting gain to 0.9 
+        source.start(0);
+        source.loop = true; //loop audio so you can stare for longer \/O_O\/
+    });
 
-  // Add an event listener for the "beforeunload" event (I did this in an attempt to stop the audio clipping when exiting the window or refreshing, I dont think it works)
-  // audio_context.close() should be executed after the audio is done playing, not on window refresh/exit. but \/O_o\/
-  window.addEventListener("beforeunload", () => {
-    audio_context.close();
-  });
+    // Add an event listener for the "beforeunload" event
+    window.addEventListener("beforeunload", () => {
+        audio_context.close();
+    });
 }
 
 function draw() {
-  // If bgToggle is true set background
-  if (bgToggle) {
-    background(220);
-  }
-  //iter works when viewing locally and on safari, chrome hates it and firefox just seems to ignore it
-  // This code calculates the current iteration for an animation that moves back and forth within the screen width.
-  const modNumber = (2 * innerWidth) - 1; // Set the modulo limit to the screen width multiplied by 2 minus 1
-  let iter = ((frameCount % modNumber) + 1); // Add 1 to the iteration to avoid it being 0
-  iter = iter > innerWidth ? (modNumber - iter) : iter;
+    // If bgToggle is true set background
+    if (bgToggle) {
+        background(220);
+    }
+    // This code calculates the current iteration for an animation that moves back and forth within the screen width.
+    const modNumber = (2 * innerWidth) - 1; // Set the modulo limit to the screen width multiplied by 2 minus 1
+    let iter = ((frameCount % modNumber) + 1); // Add 1 to the iteration to avoid it being 0
+    iter = iter > innerWidth ? (modNumber - iter) : iter;
 
-  // Set the ellipse to have no outline
-  noStroke();
+    // Set the ellipse to have no outline
+    noStroke();
 
-  //Calculate the diameter of the ellipse
-  let diameter = 100 + 50 * noise(angle + 20);
+    //Calculate the diameter of the ellipse
+    let diameter = 100 + 50 * noise(angle + 20);
 
-  // Calculate the ellipse coordinates
-  let x = width / 2 + 50 * noise(angle);
-  let y = height / 2 + 50 * noise(angle + 10);
+    // Calculate the ellipse coordinates
+    let x = width / 2 + 50 * noise(angle);
+    let y = height / 2 + 50 * noise(angle + 10);
 
-  // Set ellipse mode to the center
-  ellipseMode(CENTER);
+    // Set ellipse mode to the center
+    ellipseMode(CENTER);
 
-  // Apply color using different wave frequencies
-  let r = 255 * 0.5 * (1 + sin(frameCount * 0.01));
-  let g = 255 * 0.5 * (1 + sin(frameCount * 0.02));
-  let b = 255 * 0.5 * (1 + sin(frameCount * 0.03));
-  fill(r, g, b);
-  // Recursively draw the ellipse
-  recursive_ellipse(x, y, iter, diameter);
-  // Update angle for noise used in ellipse calculations
-  angle += 0.01;
+    // Apply color using different wave frequencies
+    let r = 255 * 0.5 * (1 + sin(frameCount * 0.01));
+    let g = 255 * 0.5 * (1 + sin(frameCount * 0.02));
+    let b = 255 * 0.5 * (1 + sin(frameCount * 0.03));
+    fill(r, g, b);
+    // Recursively draw the ellipse
+    recursive_ellipse(x, y, iter, diameter);
+    // Update angle for noise used in ellipse calculations
+    angle += 0.01;
 }
+
 
  
 
